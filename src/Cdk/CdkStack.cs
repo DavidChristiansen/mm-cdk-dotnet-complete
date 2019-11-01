8a8e544cab081107e7a3cdb1587e967fb9b26887
David Christiansen <coding@davedoes.net>
2019-11-01 16:20:28 +0000

    Initial commit

---
+++ src/Cdk/CdkStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace Cdk
+{
+    public class CdkStack : Stack
+    {
+        public CdkStack(Construct parent, string id, IStackProps props) : base(parent, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
---
+++ src/Cdk/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cdk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new App(null);
+            new CdkStack(app, "CdkStack", new StackProps());
+            app.Synth();
+        }
+    }
+}
